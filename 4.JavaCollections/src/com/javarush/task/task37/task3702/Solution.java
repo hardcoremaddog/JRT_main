376b4669ae83dccc0b49ba9dd04f9e29ae993f6f
Alexey <angerfist.aka.tr1ton@gmail.com>
2019-04-10 13:43:14 +0300

    add solution for task3702 (3)

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -3,7 +3,6 @@ package com.javarush.task.task37.task3702;
 import com.javarush.task.task37.task3702.male.MaleFactory;
 
 public class Solution {
-
 	public static void main(String[] args) {
 		MaleFactory factory = new MaleFactory();
 		System.out.println(factory.getPerson(99));
--- 4.JavaCollections/src/com/javarush/task/task37/task3702/female/FemaleFactory.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/female/FemaleFactory.java
@@ -1,22 +1,22 @@
-package com.javarush.task.task37.task3702.male;
+package com.javarush.task.task37.task3702.female;
 
 import com.javarush.task.task37.task3702.Human;
 
-public class MaleFactory {
+public class FemaleFactory {
 
 	public Human getPerson(int age) {
 		if (age > 0 && age < 13) {
-			return new KidBoy();
+			return new KidGirl();
 		}
 
 		else if (age > 12 && age < 20) {
-			return new TeenBoy();
+			return new TeenGirl();
 		}
 
 		else if (age > 19) {
-			return new Man();
+			return new Woman();
 		}
-		
+
 		return null;
 	}
 }
