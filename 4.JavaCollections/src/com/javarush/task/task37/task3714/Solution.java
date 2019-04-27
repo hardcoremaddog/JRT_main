7bb965acfaab5d5f18d05dc319a3ed96ecbf3169
Alexey <angerfist.aka.tr1ton@gmail.com>
2019-04-27 12:43:22 +0300

    add solution for task3714

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3714/Solution.java
@@ -0,0 +1,75 @@
+package com.javarush.task.task37.task3714;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Древний Рим
+*/
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input a roman number to be converted to decimal: ");
+        String romanString = bufferedReader.readLine();
+        System.out.println("Conversion result equals " + romanToInteger(romanString));
+    }
+
+
+    public static int value(char r) {
+        if (r == 'I')
+            return 1;
+        if (r == 'V')
+            return 5;
+        if (r == 'X')
+            return 10;
+        if (r == 'L')
+            return 50;
+        if (r == 'C')
+            return 100;
+        if (r == 'D')
+            return 500;
+        if (r == 'M')
+            return 1000;
+        return -1;
+    }
+
+    public static int romanToInteger(String s) {
+        // Initialize result
+        int res = 0;
+
+        for (int i=0; i<s.length(); i++)
+        {
+            // Getting value of symbol s[i]
+            int s1 = value(s.charAt(i));
+
+            // Getting value of symbol s[i+1]
+            if (i+1 <s.length())
+            {
+                int s2 = value(s.charAt(i+1));
+
+                // Comparing both values
+                if (s1 >= s2)
+                {
+                    // Value of current symbol is greater
+                    // or equalto the next symbol
+                    res = res + s1;
+                }
+                else
+                {
+                    res = res + s2 - s1;
+                    i++; // Value of current symbol is
+                    // less than the next symbol
+                }
+            }
+            else
+            {
+                res = res + s1;
+                i++;
+            }
+        }
+
+        return res;
+    }
+}
