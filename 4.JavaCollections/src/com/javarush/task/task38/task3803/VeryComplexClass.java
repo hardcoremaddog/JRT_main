e8d548b0e6284176356d3d6dc684eb0f6b89a0ac
Alexey <angerfist.aka.tr1ton@gmail.com>
2019-04-30 11:25:04 +0300

    add solution for task3802, task done!

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3803/VeryComplexClass.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task38.task3803;
+
+/* 
+Runtime исключения (unchecked exception)
+*/
+
+import com.sun.org.apache.xpath.internal.operations.String;
+
+public class VeryComplexClass {
+    public void methodThrowsClassCastException() {
+    	Object x = 55;
+    	String s = (String)x;
+    }
+
+    public void methodThrowsNullPointerException() {
+    	Object x = null;
+		x.equals(new Object());
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
