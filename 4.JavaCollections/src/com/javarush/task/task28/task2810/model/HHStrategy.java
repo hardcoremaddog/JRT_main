ee9c15aa0c1ec8c59a2a21c5f6d1740045927aa4
Alexey <angerfist.aka.tr1ton@gmail.com>
2019-05-04 00:28:59 +0300

    add solution for task2810 (6)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,7 +1,11 @@
 package com.javarush.task.task28.task2810.model;
 
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +16,32 @@ public class HHStrategy implements Strategy {
 	@Override
 	public List<Vacancy> getVacancies(String searchString) {
 		List<Vacancy> vacancies = new ArrayList<>();
+
+		if (searchString != null) {
+			Document doc = null;
+			try {
+				doc = Jsoup.connect(URL_FORMAT).get();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			Elements links = doc.select("a[href]");
+
+
+
+
+
+		}
 		return vacancies;
+	}
+
+	private static void print(String msg, Object... args) {
+		System.out.println(String.format(msg, args));
+	}
+
+	private static String trim(String s, int width) {
+		if (s.length() > width)
+			return s.substring(0, width-1) + ".";
+		else
+			return s;
 	}
 }
--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/Strategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/Strategy.java
@@ -1,7 +1,6 @@
 package com.javarush.task.task28.task2810.model;
 
 import com.javarush.task.task28.task2810.vo.Vacancy;
-
 import java.util.List;
 
 public interface Strategy {
