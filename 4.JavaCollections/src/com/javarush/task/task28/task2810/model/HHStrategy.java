596dd43eb478c7c77995f4e8631602d638cd8e92
Alexey <angerfist.aka.tr1ton@gmail.com>
2019-05-04 16:31:15 +0300

    add solution for task2810 (8)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -4,6 +4,7 @@ import com.javarush.task.task28.task2810.vo.Vacancy;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -12,41 +13,55 @@ import java.util.List;
 
 public class HHStrategy implements Strategy {
 	private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%d";
-
-
+	private static final String CACHED_URL = "https://javarush.ru/testdata/big28data.html";
+	private int PAGE_VALUE = 0;
 
 	@Override
 	public List<Vacancy> getVacancies(String searchString) {
 		List<Vacancy> vacancies = new ArrayList<>();
 
-		if (searchString != null) {
-			Document doc = null;
-			try {
-				Connection connection = Jsoup.connect(URL_FORMAT);
-				doc = connection
-						.userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36")
-						.referrer("").get();
-			} catch (IOException e) {
-				e.printStackTrace();
+		Document doc = null;
+
+		try {
+			doc = getDocument(searchString, PAGE_VALUE);
+			while (true) {
+				Elements elements = doc.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy");
+				if (elements.size() == 0) {
+					PAGE_VALUE = 0;
+					break;
+				}
+				for (Element element : elements) {
+					if (element != null) {
+						Vacancy vac = new Vacancy();
+						vac.setTitle(element.getElementsByAttributeValueContaining("data-qa", "title").text());
+						vac.setCity(element.getElementsByAttributeValueContaining("data-qa", "address").text());
+						vac.setCompanyName(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-employer").text());
+						vac.setSiteName(URL_FORMAT);
+						String urlPage = element.getElementsByAttributeValueContaining("data-qa", "title").attr("href");
+						vac.setUrl(urlPage);
+						String salary = element.getElementsByAttributeValueContaining("data-qa", "compensation").text();
+						vac.setSalary(salary.length() == 0 ? "" : salary);
+						vacancies.add(vac);
+					}
+				}
+				PAGE_VALUE++;
+				doc = getDocument(searchString, PAGE_VALUE);
 			}
-			Elements links = doc.select("a[href]");
-
-
-
-
-
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		return vacancies;
 	}
 
-	private static void print(String msg, Object... args) {
-		System.out.println(String.format(msg, args));
-	}
-
-	private static String trim(String s, int width) {
-		if (s.length() > width)
-			return s.substring(0, width-1) + ".";
-		else
-			return s;
+	protected Document getDocument(String searchString, int page) throws IOException {
+		if (searchString != null) {
+			String url = String.format(URL_FORMAT, searchString, page);
+			Connection connection = Jsoup.connect(url);
+			Document doc = connection
+					.userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36")
+					.referrer("no-referrer-when-downgrade").get();
+			return doc;
+		}
+		return null;
 	}
 }
