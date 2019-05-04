dfe41532fe6711c031c9569173f6765d50781def
Alexey <angerfist.aka.tr1ton@gmail.com>
2019-05-04 14:16:37 +0300

    add solution for task2810 (7)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task28.task2810.model;
 
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -13,6 +14,7 @@ public class HHStrategy implements Strategy {
 	private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%d";
 
 
+
 	@Override
 	public List<Vacancy> getVacancies(String searchString) {
 		List<Vacancy> vacancies = new ArrayList<>();
@@ -20,7 +22,10 @@ public class HHStrategy implements Strategy {
 		if (searchString != null) {
 			Document doc = null;
 			try {
-				doc = Jsoup.connect(URL_FORMAT).get();
+				Connection connection = Jsoup.connect(URL_FORMAT);
+				doc = connection
+						.userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36")
+						.referrer("").get();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
