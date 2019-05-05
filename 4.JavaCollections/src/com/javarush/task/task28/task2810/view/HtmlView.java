d46985518b4395eb25cf5e2114beaa9704fe00cd
Alexey <angerfist.aka.tr1ton@gmail.com>
2019-05-05 21:37:33 +0300

    add solution for task2810 (13)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -3,14 +3,23 @@ package com.javarush.task.task28.task2810.view;
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class HtmlView implements View {
 	private Controller controller;
+	private final String filePath = "./4.JavaCollections/src/"
+			+ this.getClass().getPackage().getName().replaceAll("\\.", "/")
+			+ "/vacancies.html";
 
 	@Override
 	public void update(List<Vacancy> vacancies) {
-		System.out.println(vacancies.size());
+		try {
+			updateFile(getUpdatedFileContent(vacancies));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -21,5 +30,17 @@ public class HtmlView implements View {
 	public void userCitySelectEmulationMethod() {
 		controller.onCitySelect("Odessa");
 
+	}
+
+	private String getUpdatedFileContent(List<Vacancy> vacancies) {
+		return null;
+	}
+
+	private void updateFile(String content) {
+		try (FileWriter writer = new FileWriter(filePath)) {
+			writer.write(content);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
