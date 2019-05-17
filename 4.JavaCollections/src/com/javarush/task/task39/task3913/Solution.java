2c8cba7ec9ffc4a1576bf3cf26eb962ac2b29acc
Alexey <angerfist.aka.tr1ton@gmail.com>
2019-05-17 19:52:48 +0300

    add solution for task3901

--- 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
@@ -1,7 +1,7 @@
 package com.javarush.task.task39.task3913;
 
 import java.nio.file.Paths;
-import java.util.Date;
+import java.sql.Date;
 
 public class Solution {
     public static void main(String[] args) {
