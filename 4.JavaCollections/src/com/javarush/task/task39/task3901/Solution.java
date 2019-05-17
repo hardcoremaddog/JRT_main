20d879d30e9b121e5bf03addbb80cc054cd88556
Alexey <angerfist.aka.tr1ton@gmail.com>
2019-05-17 20:24:25 +0300

    add solution for task3902

--- 4.JavaCollections/src/com/javarush/task/task39/task3901/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3901/Solution.java
@@ -3,8 +3,6 @@ package com.javarush.task.task39.task3901;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 /* 
 Уникальные подстроки
