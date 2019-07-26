c777f012cbd94cfa64bee5204700c3d3970edfa5
Alexey <angerfist.aka.tr1ton@gmail.com>
2019-07-26 14:41:45 +0300

    add solution for task2613(3)

---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulatorFactory.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task26.task2613;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CurrencyManipulatorFactory {
+
+    private CurrencyManipulatorFactory() {
+
+    }
+
+    private static Map<String, CurrencyManipulator> map = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);;
+
+    public static CurrencyManipulator getManipulatorByCurrencyCode(String currencyCode) {
+        if (!map.containsKey( currencyCode))
+            map.put(currencyCode, new CurrencyManipulator(currencyCode));
+
+        return map.get(currencyCode.toUpperCase());
+    }
+}
