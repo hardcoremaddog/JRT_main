783daf5892ab25ba16241f090270364ec095ef28
Alexey <angerfist.aka.tr1ton@gmail.com>
2019-07-31 13:20:22 +0300

    add solution for task2613(7)

---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/CommandExecutor.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task26.task2613.command;
+
+import com.javarush.task.task26.task2613.Operation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandExecutor {
+
+    private CommandExecutor() {
+
+    }
+
+    private static final Map<Operation, Command> allKnownCommandsMap = new HashMap<>();
+
+    static {
+        allKnownCommandsMap.put(Operation.INFO, new InfoCommand());
+        allKnownCommandsMap.put(Operation.DEPOSIT, new DepositCommand());
+        allKnownCommandsMap.put(Operation.WITHDRAW, new WithdrawCommand());
+        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
+    }
+
+    public static final void execute(Operation operation) {
+        allKnownCommandsMap.get(operation).execute();
+    }
+}
